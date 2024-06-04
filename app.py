26fdf5af804f2e1f34641c942f16bd1a8c190031
joker096 <zaartyom@yandex.ru>
2024-06-04 08:37:18 +0300

    readme

--- app.py
+++ app.py
@@ -1,87 +1,94 @@
-# pip install streamlit python-dotenv kinopoisk_dev requests
-# https://github.com/odi1n/kinopoisk_dev
-
-import os
-import requests
 import streamlit as st
+import json
+from streamlit_option_menu import option_menu
 import datetime
 
-# Загрузка переменных окружения
-from dotenv import load_dotenv
-load_dotenv()
-TOKEN = os.getenv("TOKEN")
-PLAYER_LINK = 'https://kinopoisk-watch-dsze5.ondigitalocean.app/player/'
+class MovieApp:
+    def __init__(self):
+        self.page_index = 0  # Индекс текущей страницы
 
-# Определение доступных жанров вручную
-GENRES = ["драма", "комедия", "боевик", "триллер", "фантастика", "фэнтези", "ужасы", "детектив", "мультфильм"]
+    # Функция для загрузки данных из JSON файла
+    def load_movies_from_json(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            movies_data = json.load(file)
+        return movies_data
 
-@st.experimental_fragment
-def fetch_movies(page=1, year=None, genre=None, rating=None, country=None):
-    url = "https://api.kinopoisk.dev/v1.4/movie"
-    headers = {
-        "X-API-KEY": TOKEN
-    }
-    params = {"page": page}
-    
-    # Применяем фильтры
-    if year:
-        params["year"] = year
-    if genre:
-        params["genres.name"] = genre
-    if rating:
-        params["rating.imdb"] = rating
-    if country:
-        params["releaseYears.country"] = country
-    
-    response = requests.get(url, headers=headers, params=params)
-    
-    if response.status_code == 200:
-        return response.json()
-    else:
-        st.error(f"Ошибка при получении данных: {response.status_code} - {response.text}")
-        return None
+    def filter_movies(self, movies, year=None, genre=None, title=None):
+        if year:
+            movies = [movie for movie in movies if movie.get('year') == year]
+        if genre:
+            movies = [movie for movie in movies if genre in movie.get('genres', '').split(', ')]
+        if title:
+            movies = [movie for movie in movies if title.lower() in movie.get('name', '').lower()]
+        return movies
 
-def main():
-    st.set_page_config(layout="wide")  # Устанавливаем настройки для отображения приложения на всю ширину
+    def show_movies(self, movies_data):
+        PLAYER_LINK = 'https://kinopoisk-watch-dsze5.ondigitalocean.app/player/'
 
-    st.title("Фильмы Kinopoisk")
-
-    # Поля ввода для фильтрации фильмов
-    current_year = datetime.datetime.now().year
-    year = st.selectbox("Выберите год", list(reversed(range(1900, current_year + 1))))
-    genre = st.selectbox("Выберите жанр", GENRES)
-    rating = st.slider("Выберите рейтинг", 1, 10, (5, 10))
-    country = st.text_input("Введите страну", "")
-
-    page = st.number_input("Номер страницы", value=1, min_value=1, step=1)
-
-    # Получение данных из API
-    if st.button("Получить фильмы"):
-        movies_data = fetch_movies(page, year, genre, f"{rating[0]}-{rating[1]}", country)
-
-        # Отображение полученных данных
-        if movies_data and "docs" in movies_data:
-            # Разделение доступной ширины на 6 столбцов
-            cols = st.columns(6)
-            for movie, col in zip(movies_data["docs"], cols):
-                if 'poster' in movie and movie['poster'] and 'description' in movie and movie['description']:  # Проверка наличия постера и описания
-                    # Отображение информации о фильме
+        if movies_data:
+            for i in range(0, len(movies_data), 6):  # Обработка по 6 фильмов за раз
+                cols = st.columns(6)  # Используем шесть колонок в одной строке
+                for col, movie in zip(cols, movies_data[i:i+6]):
                     try:
-                        poster_image = movie['poster']['url'] + "?width=100"  # Используем миниатюры
+                        poster_image = movie['poster_url']
+                        video_link = f"{PLAYER_LINK}?id={movie['id']}"  # Ссылка на видео просмотра
                         with col:
-                            st.image(poster_image, use_column_width='auto', output_format='JPEG', clamp=True)
-                            with st.expander(f"{movie['name']}"):
-                                st.write(movie['description'])
-                            link = f"{PLAYER_LINK}?id={movie['id']}"
-                            st.markdown(f"[Смотреть {movie['name']}]({link})")
+                            st.markdown(f"<a href='{video_link}' id='Movie_{i}'><img src='{poster_image}' width='100%'></a>", unsafe_allow_html=True)
+                            st.markdown(f"###### {movie['name']}", unsafe_allow_html=True)
                     except Exception as e:
                         st.error(f"Ошибка при отображении фильма: {e}")
-                else:
-                    pass  # Не выводим ничего, если нет постера или описания
 
+    def run(self):
+        st.set_page_config(layout="wide")  
+
+        with st.sidebar:
+            selected = option_menu(None, ["Movies", 'Series', 'Cartoons', 'Anime'],
+                                    icons=['bi bi-film', 'bi bi-tv', 'bi bi-gitlab', 'bi bi-fire'], menu_icon="bar-chart-fill", default_index=0, styles={
+                    "container": {"padding": "5!important", "background-color":'black'},
+                    "icon": {"color": "white", "font-size": "18px"},
+                    "nav-link": {"color":"white", "font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "blue"},
+                    "nav-link-selected": {"background-color": "#02ab21"},
+                })
+            
+            # Поля ввода для фильтрации фильмов
+            current_year = datetime.datetime.now().year
+            year = st.selectbox("Выберите год", [None] + list(reversed(range(1900, current_year + 1))))
+            genre = st.selectbox("Выберите жанр", [None] + ["драма", "комедия", "боевик", "триллер", "фантастика", "фэнтези", "ужасы", "детектив", "мультфильм"])
+            title = st.text_input("Введите название фильма")
+
+            # # Кнопки для пагинации
+            # if st.button("← Назад"):
+            #     if self.page_index > 0:
+            #         self.page_index -= 1
+            # if st.button("Вперед →"):
+            #     self.page_index += 1
+
+            # # Показать номер текущей страницы
+            # st.text(f"Страница: {self.page_index + 1}")
+
+        if selected == 'Movies':
+            movies_data = self.load_movies_from_json("movies.json")
+        elif selected == 'Series':
+            movies_data = self.load_movies_from_json("series.json")
+        elif selected == 'Cartoons':
+            movies_data = self.load_movies_from_json("cartoons.json")
+        elif selected == 'Anime':
+            movies_data = self.load_movies_from_json("anime.json")
         else:
-            st.write("Нет данных для отображения.")
+            movies_data = self.load_movies_from_json("movies.json")
 
+        # Применение фильтров
+        filtered_movies = self.filter_movies(movies_data, year, genre, title)
+        
+        # Пагинация
+        movies_per_page = 120
+        start_index = self.page_index * movies_per_page
+        end_index = start_index + movies_per_page
+        movies_to_display = filtered_movies[start_index:end_index]
+
+        # Отображение фильмов
+        self.show_movies(movies_to_display)
 
 if __name__ == "__main__":
-    main()
+    app = MovieApp()
+    app.run()
